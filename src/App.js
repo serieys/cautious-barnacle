b661a0522e21819d036d2ef397dc77b68250ce83
serieys <serieys.t@gmail.com>
2017-10-16 17:41:34 +0200

    mend

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import SearchBar from './components/SearchBar'
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: ''
+    };
+    this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+  }
+  handleFilterTextChange(filterText) {
+    this.setState({
+      filterText: filterText
+    });
+  }
+ 
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          onFilterTextChange={this.handleFilterTextChange}
+        />
+        <p>{this.state.filterText}</p>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
